5fa1ad26a392f5ffa328b111f0f0a8f0f6801379
Jorge Flordelis <fpjorge93@gmail.com>
2021-02-08 03:08:17 -0800

    ws_client store_array

--- calculator-path/ws-client/forclient.py
+++ calculator-path/ws-client/forclient.py
@@ -7,9 +7,9 @@ import websockets
 import json
 from contextlib import suppress
 
-store_array = []
 
-async def hello():  
+async def hello():
+    store_array = []
     uri = "ws://0.0.0.0:8080"
     async with websockets.connect(uri) as websocket:
         with open('./AreaFSPL.json') as f: 
@@ -17,27 +17,22 @@ async def hello():
         # print(json.dumps(data, indent=4, sort_keys=True))
         await websocket.send(json.dumps(data))
 
+        for item in 
         while True:
             greeting = await websocket.recv()
             data = json.loads(greeting) # Dictionary
             if data["type"] == "partial":
                 store_array.append(data["result"])
-                # print(store_array) 
-                # print("---------------------")
                 # print(json.dumps(data["result"], indent=3)) # JSON string
                 #print(json.dumps(data["fflush"], indent=3))
                 # j_out.update(data["result"])
                 # print(json.dumps(data, indent=3, sort_keys=True)) 
             await asyncio.sleep(1)            
-        
-async def printstorearray():
-    print(store_array)
+        print(store_array) 
 
 async def main():
     asyncio.ensure_future(hello())  # fire and forget
     await asyncio.sleep(6)
-    await printstorearray()
-    # await asyncio.sleep(6)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
